430808e73e83afbf092dbc0942530567229c1228
NU-Ring <147471894+NU-Ring@users.noreply.github.com>
2024-02-02 17:44:47 +0900

    [Gold V] Title: 최소비용 구하기, Time: 540 ms, Memory: 50816 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
@@ -1,75 +1,79 @@
+/*
+* 작성자, 1.0, 2024.02.02 누Ring <dierdia@naver.com>
+*
+* BAEKJOON 골드5 1916 최소비용 구하기
+* 문제 링크: https://www.acmicpc.net/problem/1916
+* 작성 언어: JAVA
+*/
 import java.util.*;
 import java.io.*;
 
-public class Main {
-
-	static class Node implements Comparable<Node>
-	{
-		int v;
-		int cost;
-		
-		public Node(int v, int cost){
-			this.v=v;
-			this.cost=cost;
-		}
-		
-		@Override
-		public int compareTo(Node node)
-		{
-			return this.cost-node.cost;
-		}
-		
-	}
+public class Main{
 	
+    static class Node implements Comparable<Node>{
+    	int v;
+        int c;
+        
+        public Node(int v,int c){
+        	this.v=v;
+            this.c=c;
+        }
+        
+        @Override
+        public int compareTo(Node n){
+        	return this.c-n.c;
+        }
+    }
+
 	public static void main(String[] args) throws Exception {
-		//System.setIn(new FileInputStream("res/input_1916_골드5_최소비용구하기"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N=Integer.parseInt(br.readLine()); // 도시의 개수 (1 ≤ N ≤ 1,000)
-		int M=Integer.parseInt(br.readLine()); // 버스의 개수 (1 ≤ M ≤ 100,000)
-		
-		ArrayList<ArrayList<Node>> list = new ArrayList<ArrayList<Node>>();
-		for(int i=0;i<=N;i++)
-		{
-			list.add(new ArrayList<Node>());
-		}
-		
 		StringTokenizer st;
-		for(int i=0;i<M;i++)
-		{
-			st=new StringTokenizer(br.readLine());
-			int start = Integer.parseInt(st.nextToken());
-			int end = Integer.parseInt(st.nextToken());
-			int cost = Integer.parseInt(st.nextToken());
-			list.get(start).add(new Node(end, cost));
-		}
-		
-		st = new StringTokenizer(br.readLine());
-		int ST = Integer.parseInt(st.nextToken());
-		int ED = Integer.parseInt(st.nextToken());
-		
-		int ans[] = new int[N+1];
-		Arrays.fill(ans,Integer.MAX_VALUE);
-		
-		
-		PriorityQueue<Node> pq = new PriorityQueue<>();
-		pq.offer(new Node(ST,0));
-		ans[ST]=0;
-		while(!pq.isEmpty())
-		{
-			Node now=pq.poll();
-            if(ans[now.v]<now.cost)
-            {
-                continue;
-            }
-			for(Node next:list.get(now.v))
+        
+        int N=Integer.parseInt(br.readLine());
+        int M=Integer.parseInt(br.readLine());
+
+        ArrayList<ArrayList<Node>> list =new ArrayList<ArrayList<Node>>();
+        for(int i=0;i<=N;i++){
+        	list.add(new ArrayList<Node>());
+        }
+        
+        for(int i=0;i<M;i++){
+        	st= new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            list.get(n).add(new Node(v,c));
+        }
+        
+        st=new StringTokenizer(br.readLine());
+        int start= Integer.parseInt(st.nextToken());
+        int end= Integer.parseInt(st.nextToken());
+        
+        
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        pq.offer(new Node(start,0));
+        
+        int[]ans = new int[N+1];
+        for(int i=0;i<=N;i++)
+	    {
+        	ans[i]=Integer.MAX_VALUE;
+        }
+        
+        ans[start]=0;
+        while(!pq.isEmpty()){
+        	Node now=pq.poll();
+            //시간초과로 인해 아래 추가
+			if(ans[now.v]<now.c)
 			{
-				if(ans[next.v]>ans[now.v]+next.cost)
-				{
-					ans[next.v]=ans[now.v]+next.cost;
-					pq.offer(new Node(next.v, ans[next.v]));
-				}
+				continue;
 			}
-		}
-		System.out.println(ans[ED]);
+            for(Node next:list.get(now.v)){
+            	if(ans[next.v]>ans[now.v]+next.c){
+                	ans[next.v]=ans[now.v]+next.c;
+                    pq.offer(new Node(next.v, ans[next.v]));
+                }
+            }
+        }
+        System.out.println(ans[end]);  
 	}
 }
