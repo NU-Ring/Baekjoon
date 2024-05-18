890e8ccb8c3b67473b8bfb6cd90c752c2fc2f8d5
NU-Ring <147471894+NU-Ring@users.noreply.github.com>
2024-05-18 11:42:36 +0900

    [Gold IV] Title: 부분합, Time: 240 ms, Memory: 23884 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.java
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -2,53 +2,50 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
 	public static void main(String[] args) throws Exception {
+		//System.setIn(new FileInputStream("res/input_골드4_1806_부분합"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st= new StringTokenizer(br.readLine());
-		int n=Integer.parseInt(st.nextToken());
-		int s=Integer.parseInt(st.nextToken());
-		int[]arr = new int[n];
-		st=new StringTokenizer(br.readLine());
-		for(int i=0;i<n;i++)
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N=Integer.parseInt(st.nextToken());
+		int S=Integer.parseInt(st.nextToken());
+		int[]arr=new int[N];
+		 st = new StringTokenizer(br.readLine());
+		for(int i=0;i<N;i++)
 		{
 			arr[i]=Integer.parseInt(st.nextToken());
 		}
+		int ans=Integer.MAX_VALUE;
+		int sum=arr[0];
 		int pt1=0;
 		int pt2=0;
-		int sum=arr[pt1];
-		
-		
-		int ans=Integer.MAX_VALUE;
+	
 		while(true)
-		{
-			if(s<=sum)
+		{			
+			if(sum>=S)
 			{
-				ans=Math.min(ans,pt2-pt1);
+				//System.out.println(pt1+" "+pt2);
+				ans=Math.min(ans,pt2-pt1+1);
+				sum-=arr[pt1];
+				pt1++;
 			}
-			if(s>sum||pt1==pt2)
+			else
 			{
 				pt2++;
-				if(pt2==n)
+				if(pt2>=N)
 				{
 					break;
 				}
 				sum+=arr[pt2];
 			}
-			else
-			{
-				sum-=arr[pt1];
-				pt1++;
-			}
+			
 		}
-		
-		if(ans!=Integer.MAX_VALUE)
+		if(ans>S)
 		{
-			System.out.println(ans+1);
+			System.out.println(0);
 		}
 		else
 		{
-			System.out.println(0);
+			System.out.println(ans);
 		}
 	}
 }
